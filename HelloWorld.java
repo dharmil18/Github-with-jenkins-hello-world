3b5a8636f3e9a597508077496b622abe04d01f1d
Dharmil <dharmilch18@gmail.com>
2020-11-21 13:00:08 +0530

    Newly Updated HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ class HelloWorld {
         System.out.println("Hello World");
         System.out.println("Welcome to Jenkins");
         System.out.println("This is the github integration with Jenkins");
+        System.out.println("Changing content so that Jenkins automatically runs after seeing a change in the Github Repository");
     }
 }
